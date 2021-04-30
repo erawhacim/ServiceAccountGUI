495360382b4420a7af44b863cb805b6823e01f8e
Micah Ware <micahware@me.com>
2021-04-30 07:23:32 -0500

    Initial Commit

---
+++ src/Globals.ps1
@@ -0,0 +1,603 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+$global:FormVersion = "1.3.1"
+$global:Author = "Micah Ware"
+$global:AuthorEmail = "micahware@gmail.com"
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{
+<#
+	.SYNOPSIS
+		Get-ScriptDirectory returns the proper location of the script.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+	[OutputType([string])]
+	param ()
+	if ($null -ne $hostinvocation)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+#Sample variable that provides the location of the script
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+function GenerateStrongPassword
+{
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[int]$PasswordLength
+	)
+	
+	Add-Type -AssemblyName System.Web
+	
+	$PassComplexCheck = $false
+	
+	do
+	{
+		$newPassword = [System.Web.Security.Membership]::GeneratePassword($PasswordLength, 1)
+		
+		If (
+			($newPassword -cmatch "[A-Z\p{Lu}\s]") `
+			-and ($newPassword -cmatch "[a-z\p{Ll}\s]") `
+			-and ($newPassword -match "[\d]") `
+			-and ($newPassword -match "[^\w]")
+		)
+		{
+			$PassComplexCheck = $True
+		}
+	}
+	While (
+		$PassComplexCheck -eq $false
+	)
+	
+	return $newPassword
+}
+
+function Update-ComboBox
+{
+	<#
+	.SYNOPSIS
+		This functions helps you load items into a ComboBox.
+	
+	.DESCRIPTION
+		Use this function to dynamically load items into the ComboBox control.
+	
+	.PARAMETER ComboBox
+		The ComboBox control you want to add items to.
+	
+	.PARAMETER Items
+		The object or objects you wish to load into the ComboBox's Items collection.
+	
+	.PARAMETER DisplayMember
+		Indicates the property to display for the items in this control.
+	
+	.PARAMETER ValueMember
+		Indicates the property to use for the value of the control.
+	
+	.PARAMETER Append
+		Adds the item(s) to the ComboBox without clearing the Items collection.
+	
+	.EXAMPLE
+		Update-ComboBox $combobox1 "Red", "White", "Blue"
+	
+	.EXAMPLE
+		Update-ComboBox $combobox1 "Red" -Append
+		Update-ComboBox $combobox1 "White" -Append
+		Update-ComboBox $combobox1 "Blue" -Append
+	
+	.EXAMPLE
+		Update-ComboBox $combobox1 (Get-Process) "ProcessName"
+	
+	.NOTES
+		Additional information about the function.
+#>
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNull()]
+		[System.Windows.Forms.ComboBox]$ComboBox,
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNull()]
+		$Items,
+		[Parameter(Mandatory = $false)]
+		[string]$DisplayMember,
+		[Parameter(Mandatory = $false)]
+		[string]$ValueMember,
+		[switch]$Append
+	)
+	
+	if (-not $Append)
+	{
+		$ComboBox.Items.Clear()
+	}
+	
+	if ($Items -is [Object[]])
+	{
+		$ComboBox.Items.AddRange($Items)
+	}
+	elseif ($Items -is [System.Collections.IEnumerable])
+	{
+		$ComboBox.BeginUpdate()
+		foreach ($obj in $Items)
+		{
+			$ComboBox.Items.Add($obj)
+		}
+		$ComboBox.EndUpdate()
+	}
+	else
+	{
+		$ComboBox.Items.Add($Items)
+	}
+	
+	if ($DisplayMember)
+	{
+		$ComboBox.DisplayMember = $DisplayMember
+	}
+	
+	if ($ValueMember)
+	{
+		$ComboBox.ValueMember = $ValueMember
+	}
+}
+
+function Update-DataGridView
+{
+	<#
+	.SYNOPSIS
+		This functions helps you load items into a DataGridView.
+
+	.DESCRIPTION
+		Use this function to dynamically load items into the DataGridView control.
+
+	.PARAMETER  DataGridView
+		The DataGridView control you want to add items to.
+
+	.PARAMETER  Item
+		The object or objects you wish to load into the DataGridView's items collection.
+	
+	.PARAMETER  DataMember
+		Sets the name of the list or table in the data source for which the DataGridView is displaying data.
+
+	.PARAMETER AutoSizeColumns
+	    Resizes DataGridView control's columns after loading the items.
+	#>
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	Param (
+		[ValidateNotNull()]
+		[Parameter(Mandatory = $true)]
+		[System.Windows.Forms.DataGridView]$DataGridView,
+		[ValidateNotNull()]
+		[Parameter(Mandatory = $true)]
+		$Item,
+		[Parameter(Mandatory = $false)]
+		[string]$DataMember,
+		[System.Windows.Forms.DataGridViewAutoSizeColumnsMode]$AutoSizeColumns = 'None'
+	)
+	$DataGridView.SuspendLayout()
+	$DataGridView.DataMember = $DataMember
+	
+	if ($null -eq $Item)
+	{
+		$DataGridView.DataSource = $null
+	}
+	elseif ($Item -is [System.Data.DataSet] -and $Item.Tables.Count -gt 0)
+	{
+		$DataGridView.DataSource = $Item.Tables[0]
+	}
+	elseif ($Item -is [System.ComponentModel.IListSource]`
+		-or $Item -is [System.ComponentModel.IBindingList] -or $Item -is [System.ComponentModel.IBindingListView])
+	{
+		$DataGridView.DataSource = $Item
+	}
+	else
+	{
+		$array = New-Object System.Collections.ArrayList
+		
+		if ($Item -is [System.Collections.IList])
+		{
+			$array.AddRange($Item)
+		}
+		else
+		{
+			$array.Add($Item)
+		}
+		$DataGridView.DataSource = $array
+	}
+	
+	if ($AutoSizeColumns -ne 'None')
+	{
+		$DataGridView.AutoResizeColumns($AutoSizeColumns)
+	}
+	
+	$DataGridView.ResumeLayout()
+}
+
+function ConvertTo-DataTable
+{
+	<#
+		.SYNOPSIS
+			Converts objects into a DataTable.
+	
+		.DESCRIPTION
+			Converts objects into a DataTable, which are used for DataBinding.
+	
+		.PARAMETER  InputObject
+			The input to convert into a DataTable.
+	
+		.PARAMETER  Table
+			The DataTable you wish to load the input into.
+	
+		.PARAMETER RetainColumns
+			This switch tells the function to keep the DataTable's existing columns.
+		
+		.PARAMETER FilterCIMProperties
+			This switch removes CIM properties that start with an underline.
+	
+		.EXAMPLE
+			$DataTable = ConvertTo-DataTable -InputObject (Get-Process)
+	#>
+	[OutputType([System.Data.DataTable])]
+	param (
+		$InputObject,
+		[ValidateNotNull()]
+		[System.Data.DataTable]$Table,
+		[switch]$RetainColumns,
+		[switch]$FilterCIMProperties)
+	
+	if ($null -eq $Table)
+	{
+		$Table = New-Object System.Data.DataTable
+	}
+	
+	if ($null -eq $InputObject)
+	{
+		$Table.Clear()
+		return @( ,$Table)
+	}
+	
+	if ($InputObject -is [System.Data.DataTable])
+	{
+		$Table = $InputObject
+	}
+	elseif ($InputObject -is [System.Data.DataSet] -and $InputObject.Tables.Count -gt 0)
+	{
+		$Table = $InputObject.Tables[0]
+	}
+	else
+	{
+		if (-not $RetainColumns -or $Table.Columns.Count -eq 0)
+		{
+			#Clear out the Table Contents
+			$Table.Clear()
+			
+			if ($null -eq $InputObject) { return } #Empty Data
+			
+			$object = $null
+			#find the first non null value
+			foreach ($item in $InputObject)
+			{
+				if ($null -ne $item)
+				{
+					$object = $item
+					break
+				}
+			}
+			
+			if ($null -eq $object) { return } #All null then empty
+			
+			#Get all the properties in order to create the columns
+			foreach ($prop in $object.PSObject.Get_Properties())
+			{
+				if (-not $FilterCIMProperties -or -not $prop.Name.StartsWith('__')) #filter out CIM properties
+				{
+					#Get the type from the Definition string
+					$type = $null
+					
+					if ($null -ne $prop.Value)
+					{
+						try { $type = $prop.Value.GetType() }
+						catch { Out-Null }
+					}
+					
+					if ($null -ne $type) # -and [System.Type]::GetTypeCode($type) -ne 'Object')
+					{
+						[void]$table.Columns.Add($prop.Name, $type)
+					}
+					else #Type info not found
+					{
+						[void]$table.Columns.Add($prop.Name)
+					}
+				}
+			}
+			
+			if ($object -is [System.Data.DataRow])
+			{
+				foreach ($item in $InputObject)
+				{
+					$Table.Rows.Add($item)
+				}
+				return @( ,$Table)
+			}
+		}
+		else
+		{
+			$Table.Rows.Clear()
+		}
+		
+		foreach ($item in $InputObject)
+		{
+			$row = $table.NewRow()
+			
+			if ($item)
+			{
+				foreach ($prop in $item.PSObject.Get_Properties())
+				{
+					if ($table.Columns.Contains($prop.Name))
+					{
+						$row.Item($prop.Name) = $prop.Value
+					}
+				}
+			}
+			[void]$table.Rows.Add($row)
+		}
+	}
+	
+	return @( ,$Table)
+}
+
+function Load-DataGridView
+{
+	<#
+	.SYNOPSIS
+		This functions helps you load items into a DataGridView.
+
+	.DESCRIPTION
+		Use this function to dynamically load items into the DataGridView control.
+
+	.PARAMETER  DataGridView
+		The DataGridView control you want to add items to.
+
+	.PARAMETER  Item
+		The object or objects you wish to load into the DataGridView's items collection.
+	
+	.PARAMETER  DataMember
+		Sets the name of the list or table in the data source for which the DataGridView is displaying data.
+
+	#>
+	Param (
+		[ValidateNotNull()]
+		[Parameter(Mandatory = $true)]
+		[System.Windows.Forms.DataGridView]$DataGridView,
+		[ValidateNotNull()]
+		[Parameter(Mandatory = $true)]
+		$Item,
+		[Parameter(Mandatory = $false)]
+		[string]$DataMember
+	)
+	$DataGridView.SuspendLayout()
+	$DataGridView.DataMember = $DataMember
+	
+	if ($Item -is [System.ComponentModel.IListSource]`
+		-or $Item -is [System.ComponentModel.IBindingList] -or $Item -is [System.ComponentModel.IBindingListView])
+	{
+		$DataGridView.DataSource = $Item
+	}
+	else
+	{
+		$array = New-Object System.Collections.ArrayList
+		
+		if ($Item -is [System.Collections.IList])
+		{
+			$array.AddRange($Item)
+		}
+		else
+		{
+			$array.Add($Item)
+		}
+		$DataGridView.DataSource = $array
+	}
+	
+	$DataGridView.ResumeLayout()
+}
+
+function ConvertFrom-DistinguishedName
+{
+    <#
+    .SYNOPSIS
+    Short description
+    .DESCRIPTION
+    Long description
+    .PARAMETER DistinguishedName
+    Parameter description
+    .PARAMETER ToOrganizationalUnit
+    Parameter description
+    .PARAMETER ToDC
+    Parameter description
+    .PARAMETER ToDomainCN
+    Parameter description
+    #>
+	[CmdletBinding()]
+	param (
+		[alias('Identity', 'DN')]
+		[Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)]
+		[string[]]$DistinguishedName,
+		[switch]$ToOrganizationalUnit,
+		[switch]$ToDC,
+		[switch]$ToDomainCN
+	)
+	process
+	{
+		foreach ($Distinguished in $DistinguishedName)
+		{
+			if ($ToDomainCN)
+			{
+				$DN = $Distinguished -replace '.*?((DC=[^=]+,)+DC=[^=]+)$', '$1'
+				$CN = $DN -replace ',DC=', '.' -replace "DC="
+				$CN
+			}
+			elseif ($ToOrganizationalUnit)
+			{
+				[Regex]::Match($Distinguished, '(?=OU=)(.*\n?)(?<=.)').Value
+			}
+			elseif ($ToDC)
+			{
+				$Distinguished -replace '.*?((DC=[^=]+,)+DC=[^=]+)$', '$1'
+			}
+			else
+			{
+				$Regex = '^CN=(?<cn>.+?)(?<!\\),(?<ou>(?:(?:OU|CN).+?(?<!\\),)+(?<dc>DC.+?))$'
+				$Output = foreach ($_ in $Distinguished)
+				{
+					$_ -match $Regex
+					$Matches
+				}
+				$Output.cn
+			}
+		}
+	}
+}
+
+function Show-InputBox
+{
+	param
+	(
+		[string]$message = $(Throw "You must enter a prompt message"),
+		[string]$title = "Input",
+		[string]$default
+	)
+	
+	[reflection.assembly]::loadwithpartialname("microsoft.visualbasic") | Out-Null
+	[microsoft.visualbasic.interaction]::InputBox($message, $title, $default)
+}
+
+function Show-MsgBox
+{
+	[CmdletBinding()]
+	param (
+		[Parameter(Position = 0, Mandatory = $true)]
+		[string]$Prompt,
+		[Parameter(Position = 1, Mandatory = $false)]
+		[string]$Title = "",
+		[Parameter(Position = 2, Mandatory = $false)]
+		[ValidateSet("Information", "Question", "Critical", "Exclamation")]
+		[string]$Icon = "Information",
+		[Parameter(Position = 3, Mandatory = $false)]
+		[ValidateSet("OKOnly", "OKCancel", "AbortRetryIgnore", "YesNoCancel", "YesNo", "RetryCancel")]
+		[string]$BoxType = "OkOnly",
+		[Parameter(Position = 4, Mandatory = $false)]
+		[ValidateSet(1, 2, 3)]
+		[int]$DefaultButton = 1
+	)
+	[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+	switch ($Icon)
+	{
+		"Question" { $vb_icon = [microsoft.visualbasic.msgboxstyle]::Question }
+		"Critical" { $vb_icon = [microsoft.visualbasic.msgboxstyle]::Critical }
+		"Exclamation" { $vb_icon = [microsoft.visualbasic.msgboxstyle]::Exclamation }
+		"Information" { $vb_icon = [microsoft.visualbasic.msgboxstyle]::Information }
+	}
+	switch ($BoxType)
+	{
+		"OKOnly" { $vb_box = [microsoft.visualbasic.msgboxstyle]::OKOnly }
+		"OKCancel" { $vb_box = [microsoft.visualbasic.msgboxstyle]::OkCancel }
+		"AbortRetryIgnore" { $vb_box = [microsoft.visualbasic.msgboxstyle]::AbortRetryIgnore }
+		"YesNoCancel" { $vb_box = [microsoft.visualbasic.msgboxstyle]::YesNoCancel }
+		"YesNo" { $vb_box = [microsoft.visualbasic.msgboxstyle]::YesNo }
+		"RetryCancel" { $vb_box = [microsoft.visualbasic.msgboxstyle]::RetryCancel }
+	}
+	switch ($Defaultbutton)
+	{
+		1 { $vb_defaultbutton = [microsoft.visualbasic.msgboxstyle]::DefaultButton1 }
+		2 { $vb_defaultbutton = [microsoft.visualbasic.msgboxstyle]::DefaultButton2 }
+		3 { $vb_defaultbutton = [microsoft.visualbasic.msgboxstyle]::DefaultButton3 }
+	}
+	$popuptype = $vb_icon -bor $vb_box -bor $vb_defaultbutton
+	$ans = [Microsoft.VisualBasic.Interaction]::MsgBox($prompt, $popuptype, $title)
+	return $ans
+}
+
+function Format-DataGridview
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		$DataGridViewName
+	)
+	
+	$DataGridViewName.Rows |
+	ForEach-Object{
+		
+		If ($_.Cells['Enabled'].Value -eq 'False')
+		{
+			$_.Cells['Enabled'] | ForEach-Object{ $_.Style.ForeColor = 'Red' }
+		}
+		elseif ($_.Cells['Enabled'].Value -eq 'True')
+		{
+			$_.Cells['Enabled'] | ForEach-Object{ $_.Style.ForeColor = 'Green' }
+		}
+		
+		If ($_.Cells['AES256 Only?'].Value -eq 'False')
+		{
+			$_.Cells['AES256 Only?'] | ForEach-Object{ $_.Style.ForeColor = 'Red' }
+		}
+		elseif ($_.Cells['AES256 Only?'].Value -eq 'True')
+		{
+			$_.Cells['AES256 Only?'] | ForEach-Object{ $_.Style.ForeColor = 'Green' }
+		}
+	}
+}
+
+function Find-ServiceAccountOU
+{
+	$result = @()
+	Try
+	{
+		$PrivilegedAccountsOUFound = Get-ADOrganizationalUnit -Filter { name -like "Privileged Account*" }
+		
+		If ($PrivilegedAccountsOUFound)
+		{
+			[array]$result = $(Get-ADOrganizationalUnit -Filter { name -like "Service*" } -SearchBase $($PrivilegedAccountsOUFound.DistinguishedName)).DistinguishedName
+		}
+		else
+		{
+			[array]$result = $(Get-ADOrganizationalUnit -Filter { name -like "Service Account*" }).DistinguishedName
+		}
+	}
+	Catch
+	{
+		Write-Verbose "Can't locate the service account OU"
+	}
+	
+	If ($result.count -gt "0")
+	{
+		return $result[0]
+	}
+	else
+	{
+		return $result
+	}
+	
+}
+
+function Validate-IsEmail ([string]$Email)
+{
+	return $Email -match "^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|" +`
+	"[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" +`
+	"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*" +`
+	"[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$"
+}
