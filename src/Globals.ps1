7814dcc7b85072beab25a9aa61ee40c48d77d46e
Micah Ware <micahware@me.com>
2021-05-13 23:11:23 -0500

    Changed order for password resets Moved the password reset to after an encryption type change.

--- src/Globals.ps1
+++ src/Globals.ps1
@@ -1,7 +1,7 @@
 ﻿#--------------------------------------------
 # Declare Global Variables and Functions here
 #--------------------------------------------
-$global:FormVersion = "1.3.1"
+$global:FormVersion = "1.3.2"
 $global:Author = "Micah Ware"
 $global:AuthorEmail = "micahware@gmail.com"
 
