c0e5f057c112c48102eaaf20193c86dbcde0c1a0
Micah Ware <micahware@icloud.com>
2022-04-01 02:16:05 -0500

    Fixed Distribution Group Membership

--- src/Globals.ps1
+++ src/Globals.ps1
@@ -1,7 +1,7 @@
 ﻿#--------------------------------------------
 # Declare Global Variables and Functions here
 #--------------------------------------------
-$global:FormVersion = "1.3.3"
+$global:FormVersion = "1.3.4"
 $global:Author = "Micah Ware"
 $global:AuthorEmail = "micahware@gmail.com"
 
